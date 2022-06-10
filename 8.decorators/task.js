aab7167f5a746eba6b6e728fc1a824fa473e6be0
ElenaMughi <elena.mughi@gmail.com>
2022-06-10 21:16:04 +0300

    lesson 8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,57 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+
+    let hash = args.join(',');
+
+    let objectInCache = cache.find(
+      (item) => item.hash === hash);
+
+    if (objectInCache) {
+      let index = cache.indexOf(objectInCache);
+      console.log("Из кэша: " + cache[index].value);
+      return "Из кэша: " + cache[index].value;
+    } else {
+      let result = func(...args);
+      let value = result;
+      cache.push({ hash, value });
+      if (cache.length > 5) {
+        cache.shift();
+      }
+      console.log("Вычисляем: " + result, cache.length);
+      return "Вычисляем: " + result;
+    }
+  }
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let timerOut;
+  let timer = false;
+  return function (...args) {
+    if (!timer) {
+      func.apply(this, args); timer = true;
+    };
+    clearTimeout(timerOut);
+    timerOn = setTimeout(
+      () => { timer = false }, ms);
+  }
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+  let timerOut;
+  let timer = false;
+  function wrapper (...args) {
+    wrapper.count = wrapper.count + 1;
+    if (!timer) {
+      func.apply(this, args); timer = true;
+    };
+    clearTimeout(timerOut);
+    timerOn = setTimeout(
+      () => { timer = false }, ms);
+  }
+  wrapper.count = 0;
+  return wrapper;
 }
